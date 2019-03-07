2b1356e4eb4dc1a2a8e5b166c13038d9689e213a
Miłosz Chodkowski <777moneymaker@MBP-Miosz.mshome.net>
2019-03-07 20:21:02 +0100

    Header

--- main.cpp
+++ main.cpp
@@ -9,107 +9,11 @@
 #include <iostream>
 #include <cstdlib>
 #include <chrono>
+#include "Sorts.h"
 
-using namespace std;
+using std::cout, std::cin, std::endl;
 
-void insertionSort(int arr[],int n){
-    for(int i=1;i<n;i++){
-        int key=arr[i]; int j = i-1;
-        while(key<arr[j] && j>=0){
-            arr[j+1]=arr[j];
-            j--;
-        }
-        arr[j+1]=key;
-    }
-}
-
-void shellSort(int arr[], int n){
-    for (int gap = n/2; gap > 0; gap /= 2)
-    {
-        for (int i = gap; i < n; i += 1)
-        {
-            int temp = arr[i];
-            int j;
-            for (j = i; j >= gap && arr[j - gap] > temp; j -= gap)
-                arr[j] = arr[j - gap];
-
-            arr[j] = temp;
-        }
-    }
-}
-
-void swap(int *x, int *y){
-    int temp = *x;
-    *x = *y;
-    *y = temp;
-}
-
-void selectionSort(int arr[], int n){
-    int i, j, min;
-
-    for (i = 0; i < n-1; i++){
-        min = i;
-        for (j = i+1; j < n; j++) {
-            if (arr[j] < arr[min])
-                min = j;
-        }
-        swap(&arr[min], &arr[i]);
-    }
-}
-
-//heapify and heapsort
-void heapify(int arr[], int n, int i) {
-    int largest = i;
-    int l = 2 * i + 1;
-    int r = 2 * i + 2;
-
-
-    if (l < n && arr[l] > arr[largest])
-        largest = l;
-
-    if (r < n && arr[r] > arr[largest])
-        largest = r;
-
-    if (largest != i) {
-        swap(&arr[i], &arr[largest]);
-        heapify(arr, n, largest);
-    }
-}
-void heapSort(int arr[], int n){
-    for (int i = n / 2 - 1; i >= 0; i--)
-        heapify(arr, n, i);
-
-    for (int i = n - 1; i >= 0; i--) {
-        swap(arr[0], arr[i]);
-        heapify(arr, i, 0);
-    }
-}
-//heapify and heapsort
-
-void quickSort(int arr[], int left, int right) {
-    int i = left, j = right;
-    int tmp;
-    int pivot = arr[right];
-    while (i <= j) {
-        while (arr[i] < pivot)
-            i++;
-        while (arr[j] > pivot)
-            j--;
-        if (i <= j) {
-            tmp = arr[i];
-            arr[i] = arr[j];
-            arr[j] = tmp;
-            i++;
-            j--;
-        }
-    }
-    if (left < j)
-        quickSort(arr, left, j);
-    if (i < right)
-        quickSort(arr, i, right);
-}
-
-int main() {
+int main(void) {
 
     while (true) {
         int size;
